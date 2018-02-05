47ba976e17c1da1465b075d47a6a35fcc24ebec1
Estevao Cruz <estevaoc@hotmail.com>
2018-02-05 16:55:31 -0200

    finalizado crud do bills

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,22 +1,50 @@
 class BillsController < ApplicationController
+  before_action :set_place, only: [:create, :new, :show, :update]
+  before_action :set_bill, only: [:update, :show]
+
   def index
+    @bills = current_user.bills
   end
 
   def new
+    @bill = Bill.new
   end
 
   def create
+    @bill = Bill.new(paid: false)
+    @bill.user = current_user
+    @bill.place = @place
+    @bill.save
+    redirect_to bill_path(@bill)
   end
 
   def edit
   end
 
   def update
+    @bill.update(bill_params)
+    redirect_to places_path(@places)
   end
 
   def show
+    @orders = @bill.orders
   end
 
   def destroy
+    @bill.destroy
+  end
+
+  private
+
+  def bill_params
+    params.require(:bill).permit(:paid)
+  end
+
+  def set_bill
+    @bill = @place.bills.find(params[:id])
+  end
+
+  def set_place
+    @place = Place.find(params[:place_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,11 @@
 Rails.application.routes.draw do
-  get 'bills/index'
-
-  get 'bills/new'
-
-  get 'bills/create'
-
-  get 'bills/edit'
-
-  get 'bills/update'
-
-  get 'bills/show'
-
-  get 'bills/destroy'
-
-  get 'places/index'
-
-  get 'places/new'
-
-  get 'places/create'
-
-  get 'places/edit'
-
-  get 'places/update'
-
-  get 'places/show'
-
-  get 'places/destroy'
-
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :places do
+    collection do
+      get 'my_places', to: "places#my_places"
+    end
+    resources :bills, only: [:new, :create, :show]
+  end
+  resources :bills, only: [:index]
 end
