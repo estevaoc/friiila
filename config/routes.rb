4480585f654944b32bbfe5ddf12932d0ecf9b74a
Estevao Cruz <estevaoc@hotmail.com>
2018-02-05 12:10:26 -0200

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  get 'bills/index'
+
+  get 'bills/new'
+
+  get 'bills/create'
+
+  get 'bills/edit'
+
+  get 'bills/update'
+
+  get 'bills/show'
+
+  get 'bills/destroy'
+
+  get 'places/index'
+
+  get 'places/new'
+
+  get 'places/create'
+
+  get 'places/edit'
+
+  get 'places/update'
+
+  get 'places/show'
+
+  get 'places/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
