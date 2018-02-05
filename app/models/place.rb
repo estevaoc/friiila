e8074d9e1fca802abd35cd1e06d9147417bd2c07
Mateus Amaral <mateuspa@gmail.com>
2018-02-05 15:25:14 -0200

    Connect to #8

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,10 @@
 class Place < ApplicationRecord
+  #references
   belongs_to :user
+  has_many :items
+  has_many :bills
+
+  #validations
+  validates :address, :place_name, uniqueness: { case_sensitive: false }
+  validates :address, :place_name, presence: true
 end
