f5911ae1bfa83fd9c31ec63ac95b71a18d91c947
Estevao Cruz <estevaoc@hotmail.com>
2018-02-07 21:25:36 -0200

    butao de alerta para criar conta, porem nao funfa cancel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,10 +2,24 @@
 //= require_tree .
 
 function my_function() {
-    var x = document.getElementById("search");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+  var x = document.getElementById("search");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function alertFunction() {
+  let value = confirm("Pode abrir a conta?");
+    if (value == true)
+  {
+    alert("Conta criada!");
+    return true;
+  }
+  else
+  {
+    alert("Conta nao criada!");
+    return false;
+  }
 }
