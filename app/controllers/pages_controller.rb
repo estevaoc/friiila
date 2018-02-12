57dae168ee74de13337cd3a5dd76b20c84dab722
Estevao Cruz <estevaoc@hotmail.com>
2018-02-12 11:49:09 -0200

    back do estabelecimentos - guest, pronto

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :stablish]
 
   def home
     if user_signed_in?
       redirect_to places_path
     end
   end
+
+  def stablish
+  end
+
 end
